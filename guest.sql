465ee07e1fb2d2ba626a4cb292fce8f0b3b337e1
Hương Lan <138674523+Lanlan1710@users.noreply.github.com>
2023-12-12 00:05:57 +0700

    update code

---
+++ guest.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `Thông tin khách tham dự` (
+        `Họ` VARCHAR(255) NOT NULL,
+        `Tên` VARCHAR(255) NOT NULL,
+        `Email` VARCHAR(255) NOT NULL,
+        `Số người tham dự` VARCHAR(100) NOT NULL,
+        `Lời nhắn` VARCHAR(100) NOT NULL,
+        `Điểm danh` VARCHAR(50) NOT NULL
+    ) ENGINE = 'MyISAM'
